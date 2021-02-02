181f67d3d43a846dd53b646ea5f0b3f811bd2488
OwlSec Co <76206755+owlsecco@users.noreply.github.com>
2021-02-02 21:03:21 +0300

    Delete webshare.js

--- layouts/partials/webshare.js
+++
@@ -1,9 +0,0 @@
-if (navigator.share) {
-  navigator.share({
-    title: 'web.dev',
-    text: 'Check out web.dev.',
-    url: 'https://web.dev/',
-  })
-    .then(() => console.log('Successful share'))
-    .catch((error) => console.log('Error sharing', error));
-}
