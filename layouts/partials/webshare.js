a917cad4f6ec67f7bc7aa4d4d0bd55877380ace8
OwlSec Co <76206755+owlsecco@users.noreply.github.com>
2021-02-02 21:00:45 +0300

    Create webshare.js

---
+++ layouts/partials/webshare.js
@@ -0,0 +1,9 @@
+if (navigator.share) {
+  navigator.share({
+    title: 'web.dev',
+    text: 'Check out web.dev.',
+    url: 'https://web.dev/',
+  })
+    .then(() => console.log('Successful share'))
+    .catch((error) => console.log('Error sharing', error));
+}
